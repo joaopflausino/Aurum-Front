24a1029dcf62a4b6e36d4d087adb8dfa379f34fa
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2022-11-10 18:57:52 -0300

    pagina login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import './login.css';
+import imagem from './Imagens/Carteira.png';
 
 
 function Login() {
   return (
     <div className='main-login'>
       <div className='esquerda-login'>
-        <h1>A melhor plataforma  </h1>
-        <h1>para acompanhar seus investimentos...</h1>
-        
-
-
+        <div className='texto1'>
+          <h1>A melhor plataforma</h1>
+          <h1>para acompanhar seus investimentos...</h1>
+        </div>
+        <div className='texto2'>
+          <h2>Notícias,</h2>
+          <h2>Cotações,</h2>
+          <h2>Simulações,</h2>
+          <h2>e muito mais!</h2>
+        </div>
+        <div className='img-carteira'>
+          <img src={imagem} alt="carteira" />
+        </div>
       </div>
 
 
       <div className='direita-login'>
         <div className='card-login'>
-          <h1>LOGIN</h1>
+          <h1>Login</h1>
           <div className='campo-texto'>
-            <label for="usuario">Usuário</label>
-            <input type="text" name="usuario" placeholder="Usuário"/>
+            
+            <input type="text" name="email" placeholder="E-mail"/>
           </div>
           <div className='campo-texto'>
-            <label for="senha">Senha</label>
             <input type="password" name="senha" placeholder="Senha"/> 
           </div>
-          <button className='botao-login'>Login</button>
+          <button className='botao-login'>Entrar</button>
         </div>
 
 
