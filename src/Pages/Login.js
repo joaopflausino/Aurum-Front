44caaaa686fb36aee484d223e07649430762d0a0
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2022-11-11 14:47:52 -0300

    pagina login feita

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -11,15 +11,20 @@ function Login() {
           <h1>A melhor plataforma</h1>
           <h1>para acompanhar seus investimentos...</h1>
         </div>
-        <div className='texto2'>
-          <h2>Notícias,</h2>
-          <h2>Cotações,</h2>
-          <h2>Simulações,</h2>
-          <h2>e muito mais!</h2>
+        <div className='baixo-esquerda'>
+          <div className='texto2'>
+              <h2>Notícias,</h2>
+              <h2>Cotações,</h2>
+              <h2>Simulações,</h2>
+              <h2>Rentabilidade,</h2>
+              <h2>e muito mais...</h2>
+          </div>
+          <div className='img-carteira'>
+            <img src={imagem} className='carteira' alt="carteira" />
+          </div>
         </div>
-        <div className='img-carteira'>
-          <img src={imagem} alt="carteira" />
-        </div>
+        
+          
       </div>
 
 
@@ -27,18 +32,18 @@ function Login() {
         <div className='card-login'>
           <h1>Login</h1>
           <div className='campo-texto'>
-            
             <input type="text" name="email" placeholder="E-mail"/>
           </div>
           <div className='campo-texto'>
             <input type="password" name="senha" placeholder="Senha"/> 
           </div>
+          <button className='botao-esqueceu-senha'>Esqueceu a senha?</button>
           <button className='botao-login'>Entrar</button>
+          <h3>Não tem conta?</h3>
+          <button className='botao-criar-conta'>Registrar Agora</button>
+
         </div>
-
-
       </div>
-
     </div>
   );
 }
