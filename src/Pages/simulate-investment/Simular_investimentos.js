3f8d2659308bf6f6a4a90013171ff3553446ea6b
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2022-11-15 16:17:09 -0300

    simular

--- src/Pages/simulate-investment/Simular_investimentos.js
+++ src/Pages/simulate-investment/Simular_investimentos.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import Navbar from '../../Components/Navbar';
+import './simulate.css';
 
 function SI() {
   useEffect(() => {
@@ -8,39 +9,38 @@ function SI() {
  }, []);
   return (
     <>
-      <Navbar />
-      <section>
-        <div className='si'>
-          <h3>Simular Investimentos</h3>
-          <small>Your money growth over time using compound interest</small>
+      <Navbar />  
+        <div className='main-si'>
+          <div className='container-si'>
+            <form className='compound-form'>
+            <div className='input-group'>
+              <label for='initialamount'>Inicial Amount</label>
+              <input type="number" id="initialamount"></input>
+            </div>
+            <div className='input-group'>
+              <label for='years'>Investment Years</label>
+              <input type="number" id="initialamount"></input>
+            </div>
+            <div className='input-group'>
+              <label for='rates'>Estimated Rate(%)</label>
+              <input type="number" id="initialamount"></input>
+            </div>
+            <div className='input-group'>
+              <label for='amount'>Compound Interval</label>
+              <select id="compound">
+                <option value='1'>Annualy</option>
+                <option value='2'>Semiannualy</option>
+                <option value='4'>Quartely</option>
+                <option value='12'>Monthly</option>
+              </select>
+            </div>
+            <div className='input-group'>
+              <button>calculate</button>
+            </div>
+            </form>
+          </div>
         </div>
-        <form className='compound-form'>
-          <div className='input-group'>
-            <label for='initialamount'>Inicial Amount</label>
-            <input type="number" id="initialamount"></input>
-          </div>
-          <div className='input-group'>
-            <label for='years'>Investment Years</label>
-            <input type="number" id="initialamount"></input>
-          </div>
-          <div className='input-group'>
-            <label for='rates'>Estimated Rate(%)</label>
-            <input type="number" id="initialamount"></input>
-          </div>
-          <div className='input-group'>
-            <label for='amount'>Compound Interval</label>
-            <select id="compound">
-              <option value='1'>Annualy</option>
-              <option value='2'>Semiannualy</option>
-              <option value='4'>Quartely</option>
-              <option value='12'>Monthly</option>
-            </select>
-          </div>
-          <div className='input-group'>
-            <button>calculate</button>
-          </div>
-        </form>
-      </section>
+        
     </>
 
   );
