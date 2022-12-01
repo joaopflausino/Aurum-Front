eab141067ba61c6c6079d066d3a7ec1d15e47624
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2022-12-01 18:53:07 -0300

    pagina pronta

--- src/Pages/stocks/Principais_acoes.js
+++ src/Pages/stocks/Principais_acoes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import Navbar from '../../Components/Navbar';
 import './stocks.css';
+import imagem_nasdak from '../../shared/images/nasdaq-logo.png';
 
 function PA() {
   useEffect(() => {
@@ -11,8 +12,16 @@ function PA() {
     <>
       <Navbar />
       <div className='main-pa'>
+        {/*⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ CIMA ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩*/}
         <div className='container-cima-pa'>
+          <h1 className='titulo-nasdaq'>Índice Nasdaq</h1>
+          <img src={imagem_nasdak} className="nasdaq-logo"/>
+          <h3 className='sub-titulo-nasdaq'>National Association of Securities Dealers Automated Quotations</h3>
+          <h3 className='texto-nasdaq'>A segunda maior bolsa de valores dos Estados Unidos na qual se  encontram 
+          grandes empresas de tecnologia como eletrônica, informática,  biotecnologia e telecomunicações.</h3>
         </div>
+
+        {/*⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ BAIXO ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩*/}
         <div className='container-baixo-pa'>
           <div class="container text-center">
             <div class="row justify-content-md-center">
