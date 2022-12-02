f5043a08ffbbd74638c14e54e8177e643e420fa9
Gustavo Aleixo <gustavoaleixo2000@gmail.com>
2022-12-02 19:49:06 -0300

    mudando as cores

--- src/Pages/stocks/Principais_acoes.js
+++ src/Pages/stocks/Principais_acoes.js
@@ -38,28 +38,57 @@ function PA() {
   let dataHoje = (dataH.getFullYear()) + "-" + ((dataH.getMonth() + 1)) + "-" + (diaH); 
   
   
-  //se der problema é isso
+  //RECEBE AS AÇÕES
   let [stock1, setStock1] = useState("");
+  let [stock2, setStock2] = useState("");
+  let [stock3, setStock3] = useState("");
+  let [stock4, setStock4] = useState("");
+  let [stock5, setStock5] = useState("");
+  let [stock6, setStock6] = useState("");
+  let [stock7, setStock7] = useState("");
+  let [stock8, setStock8] = useState("");
+  let [stock9, setStock9] = useState("");
+  let [stock10, setStock10] = useState("");
+  let [stock11, setStock11] = useState("");
+  let [stock12, setStock12] = useState("");
+  let [stock13, setStock13] = useState("");
+  let [stock14, setStock14] = useState("");
+  let [stock15, setStock15] = useState("");
+  let [stock16, setStock16] = useState("");
+  let [stock17, setStock17] = useState("");
+  let [stock18, setStock18] = useState("");
+  let [stock19, setStock19] = useState("");
+  let [stock20, setStock20] = useState("");
   
   
 
-  ///⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩ API POR AÇÃO - APPLE ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩
+  ///⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩  API POR AÇÃO  ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩
 
     async function APIGET() {
+      //⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩  APPLE  ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩
       let response = await fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=AAPL&apikey=GL7OCY3JYIA7LDPG");
       let result = await response.json();
       const stock1_price = parseFloat(result["Global Quote"]["05. price"]);
       const stock1_gain = parseFloat(result["Global Quote"]["09. change"]);
-      const stock1_variation = result["Global Quote"]["10. change percent"];
-      const stock1 = <><div>{stock1_price}</div><div className='gain-variation'><div className='stock-div-gain'>{stock1_gain}</div><div>{stock1_variation}</div></div>
-      
-      </>
-
-      //se der problema é isso
+      const stock1_variation = parseFloat(result["Global Quote"]["10. change percent"]).toFixed(2);
+      if(stock1_gain >= 0) {var stock1 = <><div className='stock-div-price'>$ {stock1_price}</div><div className='gain-variation1'><div className='stock-div-gain'>{stock1_gain}</div><div>({stock1_variation}%)</div></div></>}
+      else{var stock1 = <><div className='stock-div-price'>$ {stock1_price}</div><div className='gain-variation2'><div className='stock-div-gain'>{stock1_gain}</div><div>({stock1_variation}%)</div></div></>}
       setStock1(stock1);
+
+
+      //⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩  TESLA  ⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩⇩
+      let response2 = await fetch("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=TSLA&apikey=GL7OCY3JYIA7LDPG");
+      let result2 = await response2.json();
+      const stock2_price = parseFloat(result2["Global Quote"]["05. price"]);
+      const stock2_gain = parseFloat(result2["Global Quote"]["09. change"]);
+      const stock2_variation = parseFloat(result2["Global Quote"]["10. change percent"]).toFixed(2);
+      if(stock2_gain >= 0) {var stock2 = <><div className='stock-div-price'>$ {stock2_price}</div><div className='gain-variation1'><div className='stock-div-gain'>{stock2_gain}</div><div>({stock2_variation}%)</div></div></>}
+      else{var stock2 = <><div className='stock-div-price'>$ {stock2_price}</div><div className='gain-variation2'><div className='stock-div-gain'>{stock2_gain}</div><div>({stock2_variation}%)</div></div></>}
+      setStock2(stock2);
     }
 
-    
+
+
 
 
   useEffect(() => {
@@ -99,7 +128,16 @@ function PA() {
                   <img className='img-stock' src="https://br.advfn.com/common/images/company/N_AAPL.png"></img>
                 </div> 
               </div>
-              <div className='stock'></div>
+              <div className='stock'>
+                <div className='stock-esquerda'>
+                  <h3 className='stock-symbol'>TSLA</h3>
+                  <h3 className='stock-name'>Tesla Inc</h3>
+                  <div className='stock-num'>{stock2}</div>
+                </div>
+                <div>
+                  <img className='img-stock' src="https://www.pngplay.com/wp-content/uploads/13/Tesla-Logo-PNG-HD-Quality.png"></img>
+                </div> 
+              </div>
               <div className='stock'></div>
               <div className='stock'></div>
             </div>
