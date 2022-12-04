81c5f200d1544fb6a1fc947305917804563e89a4
joaozin046 <joaoxpp2001@hotmail.com>
2022-12-04 19:15:21 -0300

    Mudanças no meus investimentos
    Mudança na biblioteca utilizada para montar tabela e "concluir investimentos"

--- src/Pages/news/News.js
+++ src/Pages/news/News.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../Components/Navbar";
 import "./news.css";
 
 function News() {
--- src/Pages/simulate-investment/Simular_investimentos.js
+++ src/Pages/simulate-investment/Simular_investimentos.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../Components/Navbar";
 import "./simulate.css";
 
 function SI() {
